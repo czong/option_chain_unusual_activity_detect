b8f0e1cb22eaea79c664449e79a36e7fc5818e6b
czong <czong.academic@gmail.com>
2016-08-23 17:26:27 -0700

    change main-yahoo.py

--- main-yahoo.py
+++ main-yahoo.py
@@ -9,15 +9,31 @@ symbol_list = ['aapl','NTES','BABA','VRX','SBUX']
 
 for symbol in symbol_list:
     oc = Options(symbol,'yahoo')
+    temp = []
+    for expire_date in  oc.expiry_dates:
+        call = oc.get_call_data(expiry = expire_date)
+        call.reset_index(['Strike','Expiry','Type','Symbol'],inplace=True)
+        call = call.loc[call.IsNonstandard==False,:]
+        put = oc.get_put_data(expiry=expire_date)
+        put.reset_index(['Strike','Expiry','Type','Symbol'],inplace=True)
+        put = put.loc[put.IsNonstandard==False,:]
+        
+
+        call_amount = ((call.Strike+call.Last)*call.Open_Int).sum()
+        call_expect_price = call_amount/call.Open_Int.sum()
+        put_amount = ((put.Strike+put.Last)*put.Open_Int).sum()
+        put_expect_price = put_amount/put.Open_Int.sum()
+
+        option_expect_price = (call_amount+put_amount)/(call.Open_Int.sum()+put.Open_Int.sum())
+        temp.append([expire_date,call_amount,call_expect_price,put_amount,put_expect_price,option_expect_price])
+        #temp.append({'expire_date':expire_date,'call_amount':call_amount,'call_expect_price':call_expect_price,'put_amount':put_amount,'put_expect_price':put_expect_price,'option_expect_price':option_expect_price})
+
     ipdb.set_trace()
-    data = oc.get_all_data()
-    call = pd.DataFrame(oc.calls)
-    put = pd.DataFrame(oc.puts)
+    temp = pd.DataFrame(temp,columns=['expire_date','call_amount','call_expect_price','put_amount','put_expect_price','option_expect_price'])
+
 
     # call expect price
-    call_strike_price = call.strike.apply(float)
     call_premium_price = call.p.replace('-',np.NaN)
-    call_premium_price = call_premium_price.apply(float)
     call_expect_price = call_strike_price+call_premium_price
     call_open_interest = call.oi.apply(float)
     # put expect price 
